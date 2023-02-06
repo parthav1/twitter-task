6cca7124ee8f538b3c8f103dfb56dafc50993c2d
parthav1 <parthav1poudel@gmail.com>
2023-02-05 21:36:23 -0500

    -a functionality added

--- searcher.py
+++ searcher.py
@@ -8,12 +8,13 @@ def parse_args():
     parser.add_argument("data", help="Specify the filepath for the data file")
     parser.add_argument("-t", "--threshold", help="Specify minimum amount of mentions a user needs to be printed", type=int, default=-2)
     parser.add_argument("-u", "--users", help="Specify how many users to print", type=int, default=9223372036854775807)
-    parser.add_argument("-o", "--output", help="Specifiy the filepath to save the output to")
+    parser.add_argument("-o", "--output", help="Specifiy the filepath to save the full output to")
+    parser.add_argument("-a", "--add", action="store_true", help="Specifications provided will be added to the output file")
 
     args = parser.parse_args()
     return args
 
-def search(datapath, threshold=int, users=int, output=None):
+def search(datapath, threshold=int, users=int, output=None, add=bool):
     data = read_csv(datapath)
     target_col = data['fact_tweet']
 
@@ -33,16 +34,28 @@ def search(datapath, threshold=int, users=int, output=None):
             print("%s: %s" % (key, value))
             top_appearing.append(key)
 
+    stop = 1
     if output is not None:
-        with open(output, 'w') as f:
-            for key, value in fake_accs.items():
-                f.write("%s: %s\n" % (key, value))
+        if add:
+            with open(output, 'a') as f:
+                for key, value in sorted(fake_accs.items(), key=lambda kv: kv[1], reverse=True):
+                    if threshold == -2 or value > threshold:
+                        if stop > users:
+                            break
+                        stop += 1
+                        f.write("%s: %s\n" % (key, value))
+                        top_appearing.append(key)
+        else:
+            with open(output, 'a') as f:
+                for key, value in fake_accs.items():
+                    f.write("%s: %s\n" % (key, value))
 
 def main():
     search(parse_args().data, 
            parse_args().threshold, 
            parse_args().users,
-           parse_args().output)
-
+           parse_args().output,
+           parse_args().add)
+    
 if __name__ == "__main__":
     main()
